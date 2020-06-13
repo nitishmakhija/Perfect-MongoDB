fc5434e12e9d23554417dfae01090a56cdf06e00
Nitish Makhija <nitishmakhija@gmail.com>
2020-06-13 15:18:40 +0530

    ***updated pacage swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:4.2
 // Generated automatically by Perfect Assistant
-// Date: 2020-06-06 09:34:20 +0000
+// Date: 2020-06-06 11:54:24 +0000
 import PackageDescription
 
 let package = Package(
@@ -9,12 +9,14 @@ let package = Package(
 		.library(name: "PerfectMongoDB", targets: ["PerfectMongoDB"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/PerfectSideRepos/Perfect-CMongo.git", "0.1.0"..<"1.0.0"),
-		.package(url: "https://github.com/PerfectSideRepos/Perfect-CBSON.git", "0.0.0"..<"1.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", "3.0.0"..<"4.0.0")
+        .package(url: "https://github.com/PerfectSideRepos/Perfect-CMongo.git", from: "0.1.0"),
+        .package(url: "https://github.com/PerfectSideRepos/Perfect-CBSON.git", from: "0.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0")
 	],
 	targets: [
 		.target(name: "PerfectMongoDB", dependencies: ["PerfectLib"]),
 		.testTarget(name: "PerfectMongoDBTests", dependencies: ["PerfectMongoDB"])
 	]
 )
+
+
